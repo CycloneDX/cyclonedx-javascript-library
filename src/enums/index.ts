a96a5cdd284cd572ed4971ce1eaad8306366c107
Jan Kowalleck <jan.kowalleck@gmail.com>
2022-03-26 12:15:48 +0100

    wip
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

---
+++ src/enums/index.ts
@@ -0,0 +1,8 @@
+export * from "./AttachmentEncoding";
+export * from "./ComponentScope";
+export * from "./ComponentType";
+export * from "./ExternalReferenceType";
+export * from "./HashAlogorithms";
+
+
+
---
+++ tests/testFoo.js
@@ -0,0 +1,3 @@
+// test that require works as expected
+const sut = require("../")
+console.log(sut.enums.HashAlgorithm.MD5)
