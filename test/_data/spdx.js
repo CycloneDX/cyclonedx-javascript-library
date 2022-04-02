88e041039a4bf3fbdfa19dad5235ed4b50af70d2
Jan Kowalleck <jan.kowalleck@gmail.com>
2022-04-02 21:41:16 +0200

    spdx foo
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- test/_data/spdx.js
+++ test/_data/spdx.js
@@ -1,13 +1,13 @@
 const assert = require('assert');
 const fs = require('fs')
+const path = require('path')
 
-const spdxSpecEnum = JSON.parse(
-    fs.readFileSync(`${__dirname}/../../res/spdx.SNAPSHOT.schema.json`)
-).enum
+const spdxSpecEnum = JSON.parse(fs.readFileSync(path.join(
+    __dirname, '../../', 'res/spdx.SNAPSHOT.schema.json'
+))).enum
+
 assert.ok(spdxSpecEnum instanceof Array)
 assert.notEqual(spdxSpecEnum.length, 0)
 spdxSpecEnum.forEach(value => assert.strictEqual(typeof value, 'string'))
 
-module.exports = {
-    spdxSpecEnum: spdxSpecEnum
-}
+exports.spdxSpecEnum = spdxSpecEnum
