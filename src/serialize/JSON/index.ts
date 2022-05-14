2d363b25dadb8c464d632ad8f53a74fe76ce02e3
Jan Kowalleck <jan.kowalleck@gmail.com>
2022-05-14 19:30:17 +0200

    wip
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- src/serialize/JSON/index.ts
+++ src/serialize/JSON/index.ts
@@ -1,5 +1,7 @@
 export * as Types from './types'
+
 export * as Normalize from './normalize'
 export * from './serializer'
-export * as Denormalize from './denormalize'
-export * from './deserializer'
+
+// export * as Denormalize from './denormalize'
+// export * from './deserializer'
