4f36272003147f01989aaeade3751a3c38891aa2
Jan Kowalleck <jan.kowalleck@gmail.com>
2022-05-15 18:36:15 +0200

    wip
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- tests/_data/enumLoader.js
+++ tests/_data/enumLoader.js
@@ -1,8 +1,13 @@
 const fs = require('fs')
 const path = require('path')
 
-const resPath = path.resolve(__dirname, '../../res/')
+const resPath = path.resolve(__dirname, '..', '..', 'res')
 
+/**
+ * @param {string} resourceFile
+ * @param {string} definitionName
+ * @return {*}
+ */
 module.exports = (resourceFile, definitionName) => JSON.parse(
   fs.readFileSync(
     path.resolve(resPath, resourceFile)
