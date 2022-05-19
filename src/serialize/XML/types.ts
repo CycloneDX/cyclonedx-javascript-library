4f33efa13886fcf6d15368d5b9d17b0cc1c1144d
Jan Kowalleck <jan.kowalleck@gmail.com>
2022-05-19 09:20:43 +0200

    wip
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- src/serialize/XML/types.ts
+++ src/serialize/XML/types.ts
@@ -1,14 +1,28 @@
 // eslint-disable-next-line @typescript-eslint/no-namespace
 export namespace SimpleXml {
 
+  /**
+   * Must be alphanumeric.
+   * Must start with alpha.
+   * Must not contain whitespace characters.
+   */
+  type attributeName = string
+
+  /**
+   * Must be alphanumeric.
+   * Must start with alpha.
+   * Must not contain whitespace characters.
+   */
+  type elementName = string
+
   export type Text = string | number
 
   export type Unset = undefined | null
 
   export interface Element {
     type: 'element'
-    name: string
-    attributes?: { [key: string]: Text | Unset }
+    name: elementName
+    attributes?: { [key: attributeName]: Text | Unset }
     children?: Iterable<Comment | Element> | Text | Unset
   }
 
