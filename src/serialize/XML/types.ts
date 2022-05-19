158d2ef4ac402ca32e4e83403b4d77ca65d4705a
Jan Kowalleck <jan.kowalleck@gmail.com>
2022-05-19 09:17:20 +0200

    wip
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- src/serialize/XML/types.ts
+++ src/serialize/XML/types.ts
@@ -1,16 +1,20 @@
 // eslint-disable-next-line @typescript-eslint/no-namespace
 export namespace SimpleXml {
 
+  export type Text = string | number
+
+  export type Unset = undefined | null
+
   export interface Element {
     type: 'element'
     name: string
-    attributes?: { [key: string]: string | number | undefined | null }
-    children?: Iterable<Comment | Element> | string | undefined | null
+    attributes?: { [key: string]: Text | Unset }
+    children?: Iterable<Comment | Element> | Text | Unset
   }
 
   export interface Comment {
     type: 'comment'
-    text?: string | undefined | null
+    text?: Text | Unset
   }
 
 }
