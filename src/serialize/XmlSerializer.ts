9a31a1b8ab99a345fb08e3783886d9aca8db81b9
Jan Kowalleck <jan.kowalleck@gmail.com>
2022-05-26 09:43:04 +0200

    wip
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- src/serialize/XmlSerializer.ts
+++ src/serialize/XmlSerializer.ts
@@ -39,67 +39,74 @@ export abstract class BaseXmlSerializer extends BaseSerializer<SimpleXml.Element
   }
 }
 
-/**
- * XML serializer for web browsers.
- */
-export class XmlSerializerForWebBrowser extends BaseXmlSerializer {
+const XmlSerializerForWebBrowserAvailable = typeof XMLSerializer === 'function' && typeof document === 'object'
+export const XmlSerializer: undefined | typeof BaseXmlSerializer = (function (isBrowser: boolean) {
   /**
-   * @throws {Error}
-   *
-   * @private
-   * @internal
+   * XML serializer for web browsers.
    */
-  protected _serialize (
-    normalizedBom: SimpleXml.Element,
-    options: SerializeOptions = {}
-  ): string {
-    const doc = document.implementation.createDocument(null, null)
-    doc.appendChild(this.#buildElement(normalizedBom, doc))
-    // TODO: incorporate `options.space`
-    return (new XMLSerializer()).serializeToString(doc)
-  }
-
-  #getNs (element: SimpleXml.Element): string | null {
-    const ns = (element.namespace ?? element.attributes?.xmlns)?.toString() ?? ''
-    return ns.length > 0
-      ? ns
-      : null
-  }
-
-  #buildElement (element: SimpleXml.Element, doc: XMLDocument, parentNS: string | null = null): Element {
-    const ns = this.#getNs(element) ?? parentNS
-    const node: Element = ns === null
-      ? doc.createElement(element.name)
-      : doc.createElementNS(ns, element.name)
-    if (isNotUndefined(element.attributes)) {
-      this.#setAttributes(node, element.attributes)
+  class XmlSerializerForWebBrowser extends BaseXmlSerializer {
+    /**
+     * @throws {Error}
+     *
+     * @private
+     * @internal
+     */
+    protected _serialize (
+      normalizedBom: SimpleXml.Element,
+      options: SerializeOptions = {}
+    ): string {
+      const doc = document.implementation.createDocument(null, null)
+      doc.appendChild(this.#buildElement(normalizedBom, doc))
+      // TODO: incorporate `options.space`
+      return (new XMLSerializer()).serializeToString(doc)
     }
-    if (isNotUndefined(element.children)) {
-      this.#setChildren(node, element.children, ns)
-    }
-    return node
-  }
 
-  #setAttributes (node: Element, attributes: SimpleXml.ElementAttributes): void {
-    for (const [name, value] of Object.entries(attributes)) {
-      if (isNotUndefined(value) && name !== 'xmlns') {
-        node.setAttribute(name, `${value}`)
+    #getNs (element: SimpleXml.Element): string | null {
+      const ns = (element.namespace ?? element.attributes?.xmlns)?.toString() ?? ''
+      return ns.length > 0
+        ? ns
+        : null
+    }
+
+    #buildElement (element: SimpleXml.Element, doc: XMLDocument, parentNS: string | null = null): Element {
+      const ns = this.#getNs(element) ?? parentNS
+      const node: Element = ns === null
+        ? doc.createElement(element.name)
+        : doc.createElementNS(ns, element.name)
+      if (isNotUndefined(element.attributes)) {
+        this.#setAttributes(node, element.attributes)
+      }
+      if (isNotUndefined(element.children)) {
+        this.#setChildren(node, element.children, ns)
+      }
+      return node
+    }
+
+    #setAttributes (node: Element, attributes: SimpleXml.ElementAttributes): void {
+      for (const [name, value] of Object.entries(attributes)) {
+        if (isNotUndefined(value) && name !== 'xmlns') {
+          node.setAttribute(name, `${value}`)
+        }
+      }
+    }
+
+    #setChildren (node: Element, children: SimpleXml.ElementChildren, parentNS: string | null = null): void {
+      const t = typeof children
+      if (t === 'string' || t === 'number') {
+        node.textContent = `${children as SimpleXml.Text}`
+        return
+      }
+
+      for (const c of (children as Iterable<SimpleXml.Comment | SimpleXml.Element>)) {
+        if (c.type === 'element') {
+          node.appendChild(this.#buildElement(c, node.ownerDocument, parentNS))
+        }
+        // comments are not implemented, yet
       }
     }
   }
 
-  #setChildren (node: Element, children: SimpleXml.ElementChildren, parentNS: string | null = null): void {
-    const t = typeof children
-    if (t === 'string' || t === 'number') {
-      node.textContent = `${children as SimpleXml.Text}`
-      return
-    }
-
-    for (const c of (children as Iterable<SimpleXml.Comment | SimpleXml.Element>)) {
-      if (c.type === 'element') {
-        node.appendChild(this.#buildElement(c, node.ownerDocument, parentNS))
-      }
-      // comments are not implemented, yet
-    }
-  }
-}
+  return isBrowser
+    ? XmlSerializerForWebBrowser
+    : undefined
+})(XmlSerializerForWebBrowserAvailable)
