87b7fb21475c3752976ef3a167b1bf73582d68fa
Jan Kowalleck <jan.kowalleck@gmail.com>
2022-05-28 13:41:33 +0200

    wip
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- tests/_data/spdx.js
+++ tests/_data/spdx.js
@@ -1,6 +1,6 @@
+const fs = require('fs')
+const path = require('path')
 const assert = require('assert')
-const fs = require('node:fs')
-const path = require('node:path')
 
 const spdxSpecEnum = JSON.parse(fs.readFileSync(path.resolve(
   __dirname, '..', '..', 'res', 'spdx.SNAPSHOT.schema.json'
