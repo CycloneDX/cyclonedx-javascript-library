d1f2529e0d630e38e866af1216d156633e74e460
Jan Kowalleck <jan.kowalleck@gmail.com>
2022-05-28 15:45:11 +0200

    wip
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- src/models/hash.ts
+++ src/models/hash.ts
@@ -3,14 +3,14 @@ import { HashAlgorithm } from '../enums'
 // no regex for the HashContent in here. It applies at runtime of a normalization/serialization process.
 export type HashContent = string
 
-export type Hash = [
+export type Hash = readonly [
   // order matters: it must reflect [key, value] of HashRepository -
   // this way a HashRepository can be constructed from multiple Hash objects.
   algorithm: HashAlgorithm,
   content: HashContent
 ]
 
-export class HashRepository extends Map<HashAlgorithm, HashContent> {
+export class HashRepository extends Map<Hash[0], Hash[1]> {
   static compareItems (a: Hash, b: Hash): number {
     /* eslint-disable-next-line @typescript-eslint/strict-boolean-expressions -- run compares in weighted order */
     return a[0].localeCompare(b[0]) ||
