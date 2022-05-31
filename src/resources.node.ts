f1d889862a3bee2508818e1c2a64e4ad94e3b5e1
Jan Kowalleck <jan.kowalleck@gmail.com>
2022-06-01 00:44:20 +0200

    wip
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- src/resources.node.ts
+++ src/resources.node.ts
@@ -32,21 +32,21 @@ export const FILES = Object.freeze({
       [Version.v1dot2, path.resolve(ROOT, 'bom-1.2.SNAPSHOT.xsd')],
       [Version.v1dot3, path.resolve(ROOT, 'bom-1.3.SNAPSHOT.xsd')],
       [Version.v1dot4, path.resolve(ROOT, 'bom-1.4.SNAPSHOT.xsd')]
-    ]) as { [key in Version]?: string}),
+    ]) as { [key in Version]?: string }),
     JSON_SCHEMA: Object.freeze(Object.fromEntries([
       // v1.0 is not defined in JSON
       // v1.1 is not defined in JSON
       [Version.v1dot2, path.resolve(ROOT, 'bom-1.2.SNAPSHOT.schema.json')],
       [Version.v1dot3, path.resolve(ROOT, 'bom-1.3.SNAPSHOT.schema.json')],
       [Version.v1dot4, path.resolve(ROOT, 'bom-1.4.SNAPSHOT.schema.json')]
-    ]) as { [key in Version]?: string}),
+    ]) as { [key in Version]?: string }),
     JSON_STRICT_SCHEMA: Object.freeze(Object.fromEntries([
       // v1.0 is not defined in JSON
       // v1.1 is not defined in JSON
       [Version.v1dot2, path.resolve(ROOT, 'bom-1.2-strict.SNAPSHOT.schema.json')],
       [Version.v1dot3, path.resolve(ROOT, 'bom-1.3-strict.SNAPSHOT.schema.json')]
       // v1.4 is already strict - no special file here
-    ]))
+    ]) as { [key in Version]?: string })
   }),
   SPDX: Object.freeze({
     XML_SCHEMA: path.resolve(ROOT, 'spdx.SNAPSHOT.xsd'),
