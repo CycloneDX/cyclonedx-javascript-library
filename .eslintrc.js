161c4f3fea955758b204fb109b82f3d89b2a01c4
Jan Kowalleck <jan.kowalleck@gmail.com>
2022-05-31 22:24:24 +0200

    wip
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,10 @@
 // https://eslint.org/
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
   // see https://github.com/standard/ts-standard
   extends: 'standard-with-typescript',
   parserOptions: {
     project: './tsconfig.json'
-  },
-  ignorePatterns: [
-    'dist/',
-    'dist.*/',
-    'node_modules/'
-  ]
+  }
 }
