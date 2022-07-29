CycloneDX/cyclonedx-javascript-library

commit 5fd2b1865b09a1f638233ceb5341b68412fd1efa

spec1.4: omit empty `component.version` (#137)
CycloneDX spec version 1.4 made element `bom.component.version` optional.  
Therefore, serialization/normalization with this spec version will no longer render this element, when its value is empty.

src/serialize/json/normalize.ts
@@ -238,13 +238,13 @@ export class OrganizationalEntityNormalizer extends Base {
 
 export class ComponentNormalizer extends Base {
   normalize (data: Models.Component, options: NormalizerOptions): Normalized.Component | undefined {
-    return this._factory.spec.supportsComponentType(data.type)
+    const spec = this._factory.spec
+    return spec.supportsComponentType(data.type)
       ? {
           type: data.type,
           name: data.name,
           group: data.group || undefined,
-          // version fallback to string for spec < 1.4
-          version: data.version || '',
+          version: data.version ?? (spec.requiresComponentVersion ? '' : undefined),
           'bom-ref': data.bomRef.value || undefined,
           supplier: data.supplier === undefined
             ? undefined

src/serialize/xml/normalize.ts
@@ -295,12 +295,21 @@ export class OrganizationalEntityNormalizer extends Base {
 
 export class ComponentNormalizer extends Base {
   normalize (data: Models.Component, options: NormalizerOptions, elementName: string): SimpleXml.Element | undefined {
-    if (!this._factory.spec.supportsComponentType(data.type)) {
+    const spec = this._factory.spec
+    if (!spec.supportsComponentType(data.type)) {
       return undefined
     }
     const supplier: SimpleXml.Element | undefined = data.supplier === undefined
       ? undefined
       : this._factory.makeForOrganizationalEntity().normalize(data.supplier, options, 'supplier')
+    const version: SimpleXml.Element | undefined = (
+      spec.requiresComponentVersion
+        ? makeTextElement
+        : makeOptionalTextElement
+    )(
+      data.version ?? '',
+      'version'
+    )
     const hashes: SimpleXml.Element | undefined = data.hashes.size > 0
       ? {
           type: 'element',
@@ -339,11 +348,7 @@ export class ComponentNormalizer extends Base {
         makeOptionalTextElement(data.publisher, 'publisher'),
         makeOptionalTextElement(data.group, 'group'),
         makeTextElement(data.name, 'name'),
-        makeTextElement(
-          // version fallback to string for spec < 1.4
-          data.version ?? '',
-          'version'
-        ),
+        version,
         makeOptionalTextElement(data.description, 'description'),
         makeOptionalTextElement(data.scope, 'scope'),
         hashes,

src/spec.ts
@@ -52,6 +52,8 @@ export interface Protocol {
   readonly supportsDependencyGraph: boolean
 
   readonly supportsToolReferences: boolean
+
+  readonly requiresComponentVersion: boolean
 }
 
 /**
@@ -67,6 +69,7 @@ class Spec implements Protocol {
   readonly #externalReferenceTypes: ReadonlySet<ExternalReferenceType>
   readonly #supportsDependencyGraph: boolean
   readonly #supportsToolReferences: boolean
+  readonly #requiresComponentVersion: boolean
 
   constructor (
     version: Version,
@@ -76,7 +79,8 @@ class Spec implements Protocol {
     hashValuePattern: RegExp,
     externalReferenceTypes: Iterable<ExternalReferenceType>,
     supportsDependencyGraph: boolean,
-    supportsToolReferences: boolean
+    supportsToolReferences: boolean,
+    requiresComponentVersion: boolean
   ) {
     this.#version = version
     this.#formats = new Set(formats)
@@ -86,6 +90,7 @@ class Spec implements Protocol {
     this.#externalReferenceTypes = new Set(externalReferenceTypes)
     this.#supportsDependencyGraph = supportsDependencyGraph
     this.#supportsToolReferences = supportsToolReferences
+    this.#requiresComponentVersion = requiresComponentVersion
   }
 
   get version (): Version {
@@ -119,6 +124,10 @@ class Spec implements Protocol {
 
   get supportsToolReferences (): boolean {
     return this.#supportsToolReferences
+  }
+
+  get requiresComponentVersion (): boolean {
+    return this.#requiresComponentVersion
   }
 }
 
@@ -172,7 +181,8 @@ export const Spec1dot2: Readonly<Protocol> = Object.freeze(new Spec(
     ExternalReferenceType.Other
   ],
   true,
-  false
+  false,
+  true
 ))
 
 /** Specification v1.3 */
@@ -225,7 +235,8 @@ export const Spec1dot3: Readonly<Protocol> = Object.freeze(new Spec(
     ExternalReferenceType.Other
   ],
   true,
-  false
+  false,
+  true
 ))
 
 /** Specification v1.4 */
@@ -279,7 +290,8 @@ export const Spec1dot4: Readonly<Protocol> = Object.freeze(new Spec(
     ExternalReferenceType.Other
   ],
   true,
-  true
+  true,
+  false
 ))
 
 export const SpecVersionDict = Object.freeze(Object.fromEntries([

tests/_data/serializeResults/json_complex_spec1.4.json.bin
@@ -46,7 +46,6 @@
         "component": {
             "type": "library",
             "name": "Root Component",
-            "version": "",
             "bom-ref": "dummy.metadata.component"
         },
         "manufacture": {
@@ -75,7 +74,6 @@
         {
             "type": "library",
             "name": "a-component",
-            "version": "",
             "bom-ref": "a-component"
         },
         {

tests/_data/serializeResults/xml_complex_spec1.4.xml.bin
@@ -34,7 +34,6 @@
         </authors>
         <component type="library" bom-ref="dummy.metadata.component">
             <name>Root Component</name>
-            <version/>
         </component>
         <manufacture>
             <name>meta manufacture</name>
@@ -55,7 +54,6 @@
     <components>
         <component type="library" bom-ref="a-component">
             <name>a-component</name>
-            <version/>
         </component>
         <component type="library" bom-ref="dummy-component">
             <supplier>

commit 7d9c3ec7b29fcb14b75c8e4b043748d25c137264

fix: omitting version (#138)
fix omitting version - caused by #137

Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

src/serialize/json/normalize.ts
@@ -239,12 +239,15 @@ export class OrganizationalEntityNormalizer extends Base {
 export class ComponentNormalizer extends Base {
   normalize (data: Models.Component, options: NormalizerOptions): Normalized.Component | undefined {
     const spec = this._factory.spec
+    const version: string = data.version ?? ''
     return spec.supportsComponentType(data.type)
       ? {
           type: data.type,
           name: data.name,
           group: data.group || undefined,
-          version: data.version ?? (spec.requiresComponentVersion ? '' : undefined),
+          version: version.length > 0 || spec.requiresComponentVersion
+            ? version
+            : undefined,
           'bom-ref': data.bomRef.value || undefined,
           supplier: data.supplier === undefined
             ? undefined

tests/_data/models.js
@@ -46,6 +46,7 @@ module.exports.createComplexStructure = function () {
         new Models.Tool({
           vendor: 'tool vendor',
           name: 'other tool',
+          version: '', // empty string, not undefined
           externalReferences: new Models.ExternalReferenceRepository([
             new Models.ExternalReference(
               'https://cyclonedx.org/tool-center/',
@@ -156,6 +157,7 @@ module.exports.createComplexStructure = function () {
     return component
   }(new Models.Component(Enums.ComponentType.Library, 'a-component', {
     bomRef: 'a-component',
+    version: '', // empty string - not undefined
     dependencies: new Models.BomRefRepository([
       new Models.BomRef('unknown foreign ref that should not be rendered')
     ])