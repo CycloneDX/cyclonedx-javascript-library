9e9a965f2e0b501647b4d03f795dcef7d88004b9
Jan Kowalleck <jan.kowalleck@gmail.com>
2023-04-06 16:34:50 +0200

    downloader
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- tools/schema-downloader/download.cjs
+++ tools/schema-downloader/download.cjs
@@ -19,4 +19,62 @@ Copyright (c) OWASP Foundation. All Rights Reserved.
 
 'use strict'
 
-// @TODO
+const { join } = require('path')
+const { promises: { writeFile } } = require('fs')
+
+const SOURCE_ROOT = 'https://raw.githubusercontent.com/CycloneDX/specification/master/schema/'
+const TARGET_ROOT = join(__dirname, '..', '..', 'res', 'schema')
+
+const BomXsd = Object.freeze({
+  versions: ['1.0', '1.1', '1.2', '1.3', '1.4'],
+  sourcePattern: `${SOURCE_ROOT}bom-%s.xsd`,
+  targetPattern: join(TARGET_ROOT, 'bom-%s.SNAPSHOT.xsd'),
+  replace: Object.freeze({
+    'schemaLocation="http://cyclonedx.org/schema/spdx"': 'schemaLocation="spdx.SNAPSHOT.xsd"',
+    'schemaLocation="https://cyclonedx.org/schema/spdx"': 'schemaLocation="spdx.SNAPSHOT.xsd"'
+  })
+})
+
+const BomJsonLax = Object.freeze({
+  versions: ['1.2', '1.3', '1.4'],
+  sourcePattern: `${SOURCE_ROOT}bom-%s.schema.json`,
+  targetPattern: join(TARGET_ROOT, 'bom-%s.SNAPSHOT.schema.json'),
+  replace: Object.freeze({
+    'spdx.schema.json': 'spdx.SNAPSHOT.schema.json',
+    'jsf-0.82.schema.json': 'jsf-0.82.SNAPSHOT.schema.json'
+  })
+})
+
+const BomJsonStrict = Object.freeze({
+  versions: ['1.2', '1.3'],
+  sourcePattern: `${SOURCE_ROOT}bom-%s-strict.schema.json`,
+  targetPattern: join(TARGET_ROOT, 'bom-%s-strict.SNAPSHOT.schema.json'),
+  replace: BomJsonLax.replace
+})
+
+const OtherDownloadables = Object.freeze(Object.fromEntries([
+  [`${SOURCE_ROOT}spdx.schema.json`, join(TARGET_ROOT, 'spdx.SNAPSHOT.schema.json')],
+  [`${SOURCE_ROOT}spdx.xsd`, join(TARGET_ROOT, 'spdx.SNAPSHOT.xsd')],
+  [`${SOURCE_ROOT}jsf-0.82.schema.json`, join(TARGET_ROOT, 'jsf-0.82.SNAPSHOT.schema.json')]
+]))
+
+const FetchOptions = Object.freeze({ mode: 'no-cors' })
+
+for (const dSpec of [BomXsd, BomJsonLax, BomJsonStrict]) {
+  for (const version of dSpec.versions) {
+    const source = dSpec.sourcePattern.replace('%s', String(version))
+    const target = dSpec.targetPattern.replace('%s', String(version))
+    fetch(source, FetchOptions).then(res => res.text()).then(
+      /** @param {string} text */
+      text => new Promise((resolve, reject) => {
+        for (const [searchValue, replaceValue] of Object.entries(dSpec.replace)) {
+          text = text.replaceAll(searchValue, replaceValue)
+        }
+        resolve(text)
+      })).then(text => writeFile(target, text))
+  }
+}
+
+for (const [source, target] of Object.entries(OtherDownloadables)) {
+  fetch(source, FetchOptions).then(res => res.text()).then(text => writeFile(target, text))
+}
