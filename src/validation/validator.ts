91172200c0115268ad40525d490ddd53b5e7a999
Jan Kowalleck <jan.kowalleck@gmail.com>
2023-04-11 17:07:06 +0200

    prep structures
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- src/validation/validator.ts
+++ src/validation/validator.ts
@@ -17,20 +17,9 @@ SPDX-License-Identifier: Apache-2.0
 Copyright (c) OWASP Foundation. All Rights Reserved.
 */
 
-export * from './index.common'
-
-// region node-specifics
-
-export * as Builders from './builders/index.node'
-export * as Factories from './factories/index.node'
-export * as Serialize from './serialize/index.node'
-export * as Validation from './validation/index.node'
-
-/**
- * Internal, until the resources-module was finalized and shows any value
- *
- * @internal
- */
-export * as _Resources from './resources.node'
-
-// endregion node-specifics
+export interface Validator {
+  /**
+   * @throws {@link Validation.ValidationError | ValidationError} in case of validation errors
+   */
+  validate: (data: any) => void
+}
