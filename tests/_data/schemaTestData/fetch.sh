1e18a38dfb9c552ab18eda1d08c039470faa112a
Jan Kowalleck <jan.kowalleck@gmail.com>
2023-09-13 11:05:27 +0200

    fetched test data
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- tests/_data/schemaTestData/fetch.sh
+++ tests/_data/schemaTestData/fetch.sh
@@ -9,12 +9,13 @@ TEMP_DIR="$(mktemp -d)"
 LOCAL_PACKAGE="$TEMP_DIR/source_package.zip"
 
 wget -O "$LOCAL_PACKAGE" "$SOURCE_PACKAGE"
-for SCHEMA_VERSION in '1.5' '1.4' '1.3' '1.2'
+for SCHEMA_VERSION in '1.5' '1.4' '1.3' '1.2' '1.1' '1.0'
 do
   unzip -d "$TEMP_DIR" "$LOCAL_PACKAGE" "$SOURCE_DIR/$SCHEMA_VERSION/*"
   rm -rf "${THIS_DIR:?}/$SCHEMA_VERSION"
   mkdir -p "$THIS_DIR/$SCHEMA_VERSION"
-  cp -rf "$TEMP_DIR/$SOURCE_DIR/$SCHEMA_VERSION/"*.{json,xml} "$THIS_DIR/$SCHEMA_VERSION/"
+  cp -rf "$TEMP_DIR/$SOURCE_DIR/$SCHEMA_VERSION/"*.xml "$THIS_DIR/$SCHEMA_VERSION/"
+  cp -rf "$TEMP_DIR/$SOURCE_DIR/$SCHEMA_VERSION/"*.json "$THIS_DIR/$SCHEMA_VERSION/" || true
 done
 
 rm -rf "${TEMP_DIR:?}"
