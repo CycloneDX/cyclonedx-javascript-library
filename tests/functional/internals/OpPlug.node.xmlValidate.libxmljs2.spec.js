0cabf9e9bc49fd607a040df60302722735f5ba48
Jan Kowalleck <jan.kowalleck@gmail.com>
2024-06-04 18:39:46 +0200

    wip
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- tests/functional/internals/OpPlug.node.xmlValidate.libxmljs2.spec.js
+++ tests/functional/internals/OpPlug.node.xmlValidate.libxmljs2.spec.js
@@ -24,35 +24,29 @@ const {
   _Resources: Resources,
   Spec: { Version }
 } = require('../../../')
-const xmlValidate = require('../../../dist.node/_optPlug.node/xmlValidate/').default
 
-suite('internals: OpPlug.node.xmlValidate', () => {
+let xmlValidate
+try {
+  xmlValidate = require('../../../dist.node/_optPlug.node/xmlValidate/__opts/libxmljs2').default
+} catch {
+  xmlValidate = undefined
+}
+
+(xmlValidate === undefined
+  ? suite.skip
+  : suite
+)('internals: OpPlug.node.xmlValidate :: libxmljs2 ', () => {
   const schemaPath = Resources.FILES.CDX.XML_SCHEMA[Version.v1dot6]
   const validXML = '<bom xmlns="http://cyclonedx.org/schema/bom/1.6"></bom>'
   const invalidXML = '<bom> xmlns="http://cyclonedx.org/schema/bom/1.6"><unexpected/></bom>'
 
-  if (xmlValidate.fails) {
-    test('call should fail/throw', () => {
-      assert.throws(
-        () => {
-          xmlValidate(validXML, schemaPath, {})
-        },
-        (err) => {
-          assert.ok(err instanceof Error)
-          assert.match(err.message, /no XmlValidator available/i)
-          return true
-        }
-      )
-    })
-  } else {
-    test('call should return null', () => {
-      const validationError = xmlValidate(validXML, schemaPath, {})
-      assert.strictEqual(validationError, null)
-    })
+  test('call should return null', () => {
+    const validationError = xmlValidate(validXML, schemaPath, {})
+    assert.strictEqual(validationError, null)
+  })
 
-    test('call should return validationError', () => {
-      const validationError = xmlValidate(invalidXML, schemaPath, {})
-      assert.notEqual(validationError, null)
-    })
-  }
+  test('call should return validationError', () => {
+    const validationError = xmlValidate(invalidXML, schemaPath, {})
+    assert.notEqual(validationError, null)
+  })
 })
