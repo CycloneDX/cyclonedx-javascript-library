c56dac96a43360e7fb25e584dec51ec64f1861d4
Jan Kowalleck <jan.kowalleck@gmail.com>
2024-11-12 14:25:24 +0100

    wip
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- tools/code-style/eslint.config.mjs
+++ tools/code-style/eslint.config.mjs
@@ -31,7 +31,7 @@ import globals from 'globals'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-const projectRoot = path.dirname(path.dirname(__dirname))q
+const projectRoot = path.dirname(path.dirname(__dirname))
 
 /* eslint-disable jsdoc/valid-types */
 
