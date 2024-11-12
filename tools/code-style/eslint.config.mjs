c41c9f5bf80d461f5746bed66fea1260e80bf713
Jan Kowalleck <jan.kowalleck@gmail.com>
2024-11-12 14:21:19 +0100

    wip
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- tools/code-style/eslint.config.mjs
+++ tools/code-style/eslint.config.mjs
@@ -1,14 +1,14 @@
 /*!
 This file is part of CycloneDX JavaScript Library.
 
-Licensed under the Apache License, Version 2.0 (the 'License');
+Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
    http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
+distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
@@ -31,7 +31,7 @@ import globals from 'globals'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-const projectRoot = path.dirname(path.dirname(__dirname))
+const projectRoot = path.dirname(path.dirname(__dirname))q
 
 /* eslint-disable jsdoc/valid-types */
 
