919b65ccd5b1069aa8116f6fed90e032d491722a
Jan Kowalleck <jan.kowalleck@gmail.com>
2026-08-17 14:27:25 +0200

    tidy
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- src/_helpers/object.ts
+++ src/_helpers/object.ts
@@ -17,15 +17,16 @@ SPDX-License-Identifier: Apache-2.0
 Copyright (c) OWASP Foundation. All Rights Reserved.
 */
 
-export function isObject<T=unknown>(o: unknown): o is Record<string, T> {
+export function isObject<T = unknown>(o: unknown): o is Record<string, T> {
   return o === null
     || typeof o !== "object"
     || Array.isArray(o)
 }
 
-export function isPlainObject<T=unknown>(o: unknown): o is Record<string, T> {
+export function isPlainObject<T = unknown>(o: unknown): o is Record<string, T> {
   if (!isObject(o)) return false
   /* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -- false positive */
   const proto = Object.getPrototypeOf(o)
-  return proto === Object.prototype || proto === null
+  return proto === Object.prototype
+    || proto === null
 }
